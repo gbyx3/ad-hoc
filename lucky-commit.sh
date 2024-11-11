4ce0f025e313b76b89fc012c0194cdba743df098
Gustav Alerby <g.alerby@gmail.com>
2024-11-11 13:48:20 +0100

    Lucky commit implementation in bash

---
+++ lucky-commit.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Generate commit message to be lucky
+# Based on a X-thread "demanding" a commit hash with a specific prefix
+
+function is_hex() {
+    if [[ $1 =~ ^[0-9a-fA-F]+$ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 '<commit_message>'"
+    exit 1
+fi
+
+MATCH="ga"
+if ! is_hex $MATCH; then
+    echo "The match string is not a valid hex string"
+    echo "Converting to hex string"
+    MATCH=$(echo -n $MATCH |xxd -p)
+fi
+
+LENGTH=${#MATCH}
+MESSAGE=$1
+while true; do
+    commit_hash=$(sha1sum <<< "$MESSAGE" | cut -c1-$LENGTH)
+    if [[ $commit_hash == $MATCH ]]; then
+        echo "Lucky commit: $MESSAGE |<- "
+        exit 0
+    fi
+    MESSAGE="$MESSAGE "
+    echo "$commit_hash"
+done
